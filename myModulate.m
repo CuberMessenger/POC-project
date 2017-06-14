0afe67032691bb08141b7713e489b0caa0cd8bba
Wonda Ku <gmdaat@qq.com>
2017-06-14 11:48:30 -0400

    实现MFSK&补充8ASK
    8ASK效果不理想。

--- myModulate.m
+++ myModulate.m
@@ -8,6 +8,10 @@ if strcmp(h.Type, 'QAM')
     tmp = modulate(h1, DataSymbol);
 end
 
+if strcmp(h.Type, 'FSK')
+    tmp = fskmod(DataSymbol, str2num(h.M), log2(str2num(h.M)), 2, 32);
+end
+
 if strcmp(h.Type, 'PSK')
     if h.M == 'Q'
         h1.M = 4;
@@ -39,6 +43,32 @@ if strcmp(h.Type, 'ASK') && (h.M == '4')
                 tmp(kk) = -1/2i;
             case 3
                 tmp(kk) = -i;
+        end
+    end
+end
+
+if strcmp(h.Type, 'ASK') && (h.M == '8')
+    tmp2 = DataSymbol;
+    tmp = zeros(1, length(DataSymbol));
+    
+    for kk = 1:length(DataSymbol)
+        switch tmp2(kk)
+            case 0
+                tmp(kk) = 1i;
+            case 1
+                tmp(kk) = 3/4i;
+            case 2
+                tmp(kk) = 1/2i;
+            case 3
+                tmp(kk) = 1/4i;
+                case 4
+                tmp(kk) = -1/4i;
+            case 5
+                tmp(kk) = -1/2i;
+            case 6
+                tmp(kk) = -3/4i;
+            case 7
+                tmp(kk) = -1i;
         end
     end
 end
